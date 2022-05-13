9bb68c869f2819346f80a8dcc91034d21497d40a
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-05-13 21:03:29 +0530

    ✨feature: Metamask Integration

--- src/Pages/Mint/index.jsx
+++ src/Pages/Mint/index.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Mint.module.css";
+import { Modal } from "react-responsive-modal";
 
 import { discord, medium, twitter } from "../../Assets/images";
+import useMetaMask from "../../Hooks/metamask";
 
 export default function Mint() {
+  const { connect, disconnect, isActive, account, shouldDisable } =
+    useMetaMask();
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
   return (
     <div className={styles.mint_page}>
       <div className={styles.title_wrapper}>
@@ -33,7 +41,7 @@ export default function Mint() {
             <div className={styles.value}>3 SOL</div>
           </div>
           <div className={styles.connect_wallet_btn_wrapper}>
-            <button>
+            <button onClick={onOpenModal}>
               <span className={styles.connect_wallet_btn_text}>
                 CONNECT WALLET
               </span>
@@ -41,6 +49,32 @@ export default function Mint() {
           </div>
         </div>
       </div>
+      <Modal open={open} onClose={onCloseModal} center>
+        <div className={styles.wallet_card_wrapper}>
+          <div className={styles.wallet_card}>
+            <div className={styles.label_value_holder}>
+              <span className={styles.label}>Remaining Amount</span>
+              <div className={styles.value}>
+                6238/<span className={styles.value_divisor}>100000</span>
+              </div>
+            </div>
+            <div className={styles.horizontal_rule_wrapper}>
+              <hr />
+            </div>
+            <div className={styles.label_value_holder}>
+              <span className={styles.label}>Price</span>
+              <div className={styles.value}>3 SOL</div>
+            </div>
+            <div className={styles.connect_wallet_btn_wrapper}>
+              <button onClick={connect}>
+                <span className={styles.connect_wallet_btn_text}>
+                  CONNECT WALLET
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Modal>
 
       <div className={styles.social_media_wrapper}>
         <img src={twitter} alt="social media" />
