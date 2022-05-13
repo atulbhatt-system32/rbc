f77b2d1092c8a9559215e9e82326e3f54e1af14a
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-05-13 13:53:30 +0530

    style: mobile nav

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,25 +4,29 @@ import { twitter, discord, medium } from "../../Assets/images/index";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.nav_left}>
-        <a href="*">Roadmap</a>
-        <a href="*">Mission</a>
-        <a href="*">Team</a>
-      </div>
-      <div className={styles.nav_right}>
-        <div className={styles.mint_btn_wrap}>
-          <button>Mint</button>
+      <div className={styles.navbar_mobile}></div>
+
+      <div className={styles.navbar_desktop}>
+        <div className={styles.nav_left}>
+          <a href="*">Roadmap</a>
+          <a href="*">Mission</a>
+          <a href="*">Team</a>
         </div>
-        <div className={styles.social_links}>
-          <a href="*">
-            <img src={twitter} alt="twitter" />
-          </a>
-          <a href="*">
-            <img src={discord} alt="discord" />
-          </a>
-          <a href="*">
-            <img src={medium} alt="medium" />
-          </a>
+        <div className={styles.nav_right}>
+          <div className={styles.mint_btn_wrap}>
+            <button>Mint</button>
+          </div>
+          <div className={styles.social_links}>
+            <a href="*">
+              <img src={twitter} alt="twitter" />
+            </a>
+            <a href="*">
+              <img src={discord} alt="discord" />
+            </a>
+            <a href="*">
+              <img src={medium} alt="medium" />
+            </a>
+          </div>
         </div>
       </div>
     </nav>
