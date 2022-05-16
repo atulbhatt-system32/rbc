6a727f951db9eb91ed8482ec50f0f3fc0fcaebda
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-05-16 11:17:05 +0530

    Wallet connect modal added for connect and not connected conditions

--- src/Pages/Mint/index.jsx
+++ src/Pages/Mint/index.jsx
@@ -56,34 +56,63 @@ export default function Mint() {
           </div>
         </div>
       </div>
-      <Modal
-        classNames={{ modal: styles.wallet_modal }}
-        // open={true}
-        open={open}
-        onClose={onCloseModal}
-        center
-        showCloseIcon={false}
-      >
-        <div className={styles.wallet_card}>
-          <div className={styles.wallet_body}>
-            <div className={styles.cross_img}>
-              <img src={cross} alt="cross" />
-            </div>
-            <div className={styles.connect_text}>
-              <span>Connect a Wallet</span>
-            </div>
-          </div>
 
+      {!!account ? (
+        <Modal
+          classNames={{ modal: styles.wallet_modal_active }}
+          open={open}
+          onClose={onCloseModal}
+          center
+          showCloseIcon={false}
+        >
           <div className={styles.wallet_logo}>
             <div className={styles.wallet_img}>
               <img src={fox} alt="fox" />
             </div>
             <div className={styles.wallet_text}>
-              <span>metamask</span>
+              <span>metamask connected</span>
             </div>
           </div>
-        </div>
-      </Modal>
+          <div className={styles.wallet_card}>
+            {isActive && (
+              <div className={styles.label_value_holder}>
+                <span className={styles.label}>{`wallet: ${account.slice(
+                  0,
+                  4
+                )}...${account.slice(-2)}`}</span>
+              </div>
+            )}
+          </div>
+        </Modal>
+      ) : (
+        <Modal
+          classNames={{ modal: styles.wallet_modal }}
+          open={open}
+          onClose={onCloseModal}
+          center
+          showCloseIcon={false}
+        >
+          <div className={styles.wallet_card}>
+            <div className={styles.wallet_body}>
+              <div className={styles.cross_img}>
+                <img src={cross} alt="cross" />
+              </div>
+              <div className={styles.connect_text}>
+                <span>Connect a Wallet</span>
+              </div>
+            </div>
+
+            <div className={styles.wallet_logo}>
+              <div className={styles.wallet_img}>
+                <img src={fox} alt="fox" />
+              </div>
+              <div className={styles.wallet_text}>
+                <span>metamask</span>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
 
       <div className={styles.social_media_wrapper}>
         <img src={twitter} alt="social media" />
@@ -95,107 +124,5 @@ export default function Mint() {
 }
 
 //
-{
-  /* <Modal
-classNames={{ modal: styles.wallet_modal }}
-open={open}
-onClose={onCloseModal}
-center
-showCloseIcon={false}
->
-<div className={styles.wallet_logo}>
-  <div className={styles.wallet_img}>
-    <img src={fox} alt="fox" />
-  </div>
-  <div className={styles.wallet_text}>
-    <span>metamask connected</span>
-  </div>
-</div>
-<div className={styles.wallet_card}>
-  {isActive && (
-    <div className={styles.label_value_holder}>
-      <span className={styles.label}>{`wallet: ${account.slice(
-        0,
-        4
-      )}...${account.slice(-2)}`}</span>
-    </div>
-  )}
-</div>
-</Modal> */
-}
 
 // css:
-// .wallet_modal {
-//   background: none;
-//   border: none;
-//   box-shadow: none;
-//   position: static;
-//   .wallet_logo {
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     padding-bottom: 1rem;
-//     .wallet_text {
-//       span {
-//         font-family: "Lora";
-//         font-style: normal;
-//         font-weight: 700;
-//         font-size: 16px;
-//         line-height: 20px;
-//         color: #000000;
-//         text-transform: capitalize;
-//       }
-//     }
-//   }
-//   .wallet_card {
-//     background-color: #fbe2e2;
-//     display: flex;
-//     justify-content: center;
-//     padding: 1rem;
-//     .label {
-//       text-align: center;
-//       font-family: "Lato", sans-serif;
-//       font-weight: 700;
-//       font-size: 22px;
-//     }
-//   }
-// }
-
-// MOBILE:
-// .wallet_modal {
-// background: none;
-// border: none;
-// box-shadow: none;
-// position: absolute;
-// top: 36%;
-// left: -18.75px;
-// .wallet_logo {
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   padding-bottom: 1rem;
-//   .wallet_text {
-//     span {
-//       font-family: "Lora";
-//       font-style: normal;
-//       font-weight: 700;
-//       font-size: 16px;
-//       line-height: 20px;
-//       color: #000000;
-//       text-transform: capitalize;
-//     }
-//   }
-// }
-// .wallet_card {
-//   background-color: #fbe2e2;
-//   display: flex;
-//   justify-content: center;
-//   padding: 1rem;
-//   .label {
-//     text-align: center;
-//     font-family: "Lato", sans-serif;
-//     font-weight: 700;
-//     font-size: 22px;
-//   }
-// }
-// }
